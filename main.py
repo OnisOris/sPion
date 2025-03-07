8280d9d135dffb07ef88f2a4dac7a982cf1b627f
Michael <onisoris@yandex.ru>
2025-03-07 19:09:47 +0300

    Увеличение силы отталкивания в 8 раз

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def main():
                 "attraction_weight": 0.5,
                 "cohesion_weight": 1.0,
                 "alignment_weight": 1.0,
-                "repulsion_weight": 4.0,
+                "repulsion_weight": 8.0,
                 "unstable_weight": 0.0,
                 "noise_weight": 1.0,
                 "safety_radius": 1.0,
