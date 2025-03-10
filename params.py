c6a666bea172924ce49ce53faf01e12aa92879f0
Michael <onisoris@yandex.ru>
2025-03-10 17:51:05 +0300

    Change weight param repulsion force

--- params.py
+++ params.py
@@ -2,7 +2,7 @@ params = {
             "attraction_weight": 0.,
             "cohesion_weight": 1.0,
             "alignment_weight": 1.0,
-            "repulsion_weight": 1.0,
+            "repulsion_weight": 0.05,
             "unstable_weight": 0.0,
             "noise_weight": 1.0,
             "safety_radius": 1.0,
